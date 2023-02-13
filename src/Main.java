65c3905c7499758dc7208113739d127cb6f7e0a2
FallenAngel888 <ricardojortigoza10@gmail.com>
2023-02-13 18:13:11 -0400

    tarea open bootcamp

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int paran1 = 1354;
+        int paran2 = 1451;
+        int paran3 = 5555;
+
+
+        var valor = suma(paran1, paran2, paran3);
+
+        System.out.println(valor);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+
+
+
